8bdcbed18c26f2f319dbe7734c304e668a1d5c91
Mikhail Shevchenko <michaelyshevchenko@gmail.com>
2020-09-14 04:32:20 +0300

    logger preconfig

--- src/main/java/com/internet/shop/controllers/IndexController.java
+++ src/main/java/com/internet/shop/controllers/IndexController.java
@@ -5,11 +5,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.apache.log4j.Logger;
 
 public class IndexController extends HttpServlet {
+    static final Logger logger = Logger.getLogger(IndexController.class);
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
+        if (logger.isDebugEnabled()) {
+            logger.debug("This is debug message. Web server is up, application started.");
+        }
         req.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(req, resp);
     }
 }
